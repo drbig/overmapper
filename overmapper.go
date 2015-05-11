dfd4775957cafd0877f5d79c934dfd159c066f6c
Piotr S. Staszewski <p.staszewski@gmail.com>
2015-05-11 17:59:38 +0200

    Saner version

--- overmapper.go
+++ overmapper.go
@@ -1,6 +1,6 @@
 // See LICENSE.txt for licensing information.
 
-package main
+package overmapper
 
 import (
 	"bufio"
@@ -9,7 +9,6 @@ import (
 	"image"
 	"image/color"
 	"image/draw"
-	"image/png"
 	"io"
 	"os"
 	"path/filepath"
@@ -19,42 +18,56 @@ import (
 )
 
 const (
-	VERSION = `1`
-	MAPX    = 180
-	MAPY    = 180
-	LEVEL   = `L 10`
-	SCALE   = 2
-
-	xo = MAPX * SCALE
-	yo = MAPY * SCALE
+	VERSION = `4`
 )
 
+const (
+	BG = iota
+	FG
+	NOTE
+	GRID
+	ORIGIN
+)
+
+type conf struct {
+	MapX, MapY   int
+	Scale, Level int
+	Colors       []*image.Uniform
+}
+
 type Map struct {
-	width, height int
-	n, s, w, e    int
-	maps          map[image.Point]string
+	Width, Height int
+	N, S, W, E    int
+	Maps          map[image.Point]string
+}
+
+func (m *Map) String() string {
+	return fmt.Sprintf("Map %dx%d (%d)", m.Width, m.Height, len(m.Maps))
 }
 
 var (
 	ErrMultiChars = errors.New("multiple characters detected")
 	ErrNotFound   = errors.New("no seen files found")
 
-	rxpSeenFile = regexp.MustCompile(`\#(.*?)\.seen\.(-?\d+)\.(-?\d+)$`)
-	CBG         = image.NewUniform(color.RGBA{0, 0, 0, 255})
-	CFG         = image.NewUniform(color.RGBA{255, 255, 255, 255})
-	CNOTE       = image.NewUniform(color.RGBA{0, 0, 255, 255})
-	CGRID       = image.NewUniform(color.RGBA{255, 0, 0, 180})
-	CORIGIN     = image.NewUniform(color.RGBA{0, 255, 0, 180})
-)
+	Config = conf{
+		180, 180,
+		2, 10,
+		[]*image.Uniform{
+			image.NewUniform(color.RGBA{0, 0, 0, 255}),
+			image.NewUniform(color.RGBA{255, 255, 255, 255}),
+			image.NewUniform(color.RGBA{0, 0, 255, 255}),
+			image.NewUniform(color.RGBA{255, 0, 0, 180}),
+			image.NewUniform(color.RGBA{0, 255, 0, 180}),
+		},
+	}
 
-func (m *Map) String() string {
-	return fmt.Sprintf("Map %dx%d (%d)", m.width, m.height, len(m.maps))
-}
+	rxpSeenFile = regexp.MustCompile(`\#(.*?)\.seen\.(-?\d+)\.(-?\d+)$`)
+)
 
 func NewMap(path string) (*Map, error) {
 	var id string
 
-	m := &Map{maps: make(map[image.Point]string)}
+	m := &Map{Maps: make(map[image.Point]string)}
 
 	err := filepath.Walk(path, func(p string, i os.FileInfo, er error) error {
 		if er != nil {
@@ -82,19 +95,19 @@ func NewMap(path string) (*Map, error) {
 				return err
 			}
 
-			m.maps[image.Point{x, y}] = p
+			m.Maps[image.Point{x, y}] = p
 
-			if x < m.w {
-				m.w = x
+			if x < m.W {
+				m.W = x
 			}
-			if x > m.e {
-				m.e = x
+			if x > m.E {
+				m.E = x
 			}
-			if y > m.n {
-				m.n = y
+			if y > m.N {
+				m.N = y
 			}
-			if y < m.s {
-				m.s = y
+			if y < m.S {
+				m.S = y
 			}
 		}
 
@@ -104,143 +117,165 @@ func NewMap(path string) (*Map, error) {
 		return nil, err
 	}
 
-	if len(m.maps) == 0 {
+	if len(m.Maps) == 0 {
 		return nil, ErrNotFound
 	}
 
-	m.width = m.e - m.w + 1
-	m.height = m.n - m.s + 1
+	m.Width = m.E - m.W + 1
+	m.Height = m.N - m.S + 1
 
 	return m, nil
 }
 
+func skipToLevel(s *bufio.Scanner) error {
+	lstr := fmt.Sprintf("L %d", Config.Level)
+
+	for s.Scan() {
+		line := s.Text()
+		if line == lstr {
+			break
+		}
+	}
+
+	return s.Err()
+}
+
+func parseSeen(i io.Reader) ([]image.Rectangle, error) {
+	var boxes []image.Rectangle
+	var visited, length, position int
+
+	for {
+		n, err := fmt.Fscanf(i, "%d %d", &visited, &length)
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		if n != 2 {
+			break
+		}
+
+		if visited == 1 {
+			x0 := position % Config.MapX
+			y0 := position / Config.MapX
+			x1 := (position + length - 1) % Config.MapX
+			y1 := (position + length - 1) / Config.MapX
+
+			if y0 == y1 {
+				boxes = append(boxes, image.Rect(x0, y0, x1, y1))
+			} else {
+				boxes = append(boxes, image.Rect(x0, y0, Config.MapX-1, y0))
+				if y0+1 != y1 {
+					boxes = append(boxes, image.Rect(0, y0+1, Config.MapX-1, y1-1))
+				}
+				boxes = append(boxes, image.Rect(0, y1, x1, y1))
+			}
+		}
+		position += length
+	}
+
+	return boxes, nil
+}
+
+func parseNotes(s *bufio.Scanner) []image.Rectangle {
+	var notes []image.Rectangle
+	var nx, ny int
+
+	for s.Scan() {
+		data := strings.NewReader(s.Text())
+		n, _ := fmt.Fscanf(data, "N %d %d", &nx, &ny)
+		if n != 2 {
+			break
+		}
+
+		notes = append(notes, image.Rect(nx, ny, nx, ny))
+
+		s.Scan()
+	}
+
+	return notes
+}
+
+func transformBox(r *image.Rectangle, x, y int) {
+	r.Min.X = (x * (Config.MapX * Config.Scale)) + (r.Min.X * Config.Scale)
+	r.Min.Y = (y * (Config.MapY * Config.Scale)) + (r.Min.Y * Config.Scale)
+	r.Max.X = (x * (Config.MapX * Config.Scale)) + ((r.Max.X + 1) * Config.Scale)
+	r.Max.Y = (y * (Config.MapY * Config.Scale)) + ((r.Max.Y + 1) * Config.Scale)
+}
+
+func drawGrid(i draw.Image, color image.Image, x, y int) {
+	var b image.Rectangle
+
+	b = image.Rect(0, 0, Config.MapX-1, 0) // top-edge
+	transformBox(&b, x, y)
+	draw.Draw(i, b, color, image.ZP, draw.Over)
+
+	b = image.Rect(0, Config.MapY-1, Config.MapX-1, Config.MapY-1) // bottom-edge
+	transformBox(&b, x, y)
+	draw.Draw(i, b, color, image.ZP, draw.Over)
+
+	b = image.Rect(0, 0, 0, Config.MapY-1) // left-edge
+	transformBox(&b, x, y)
+	draw.Draw(i, b, color, image.ZP, draw.Over)
+
+	b = image.Rect(Config.MapX-1, 0, Config.MapX-1, Config.MapY-1) // right-edge
+	transformBox(&b, x, y)
+	draw.Draw(i, b, color, image.ZP, draw.Over)
+}
+
 func (m *Map) Draw() (*image.RGBA, error) {
-	img := image.NewRGBA(image.Rect(0, 0, m.width*xo, m.height*yo))
-	draw.Draw(img, img.Bounds(), CBG, image.ZP, draw.Src)
+	img := image.NewRGBA(image.Rect(0, 0, m.Width*(Config.MapX*Config.Scale), m.Height*(Config.MapY*Config.Scale)))
+	draw.Draw(img, img.Bounds(), Config.Colors[BG], image.ZP, draw.Src)
 
 	iy := 0
-	for y := m.s; y <= m.n; y += 1 {
+	for y := m.S; y <= m.N; y += 1 {
 		ix := 0
-		for x := m.w; x <= m.e; x += 1 {
-			fmt.Printf("At %dx%d - %dx%d\n", x, y, ix, iy)
+		for x := m.W; x <= m.E; x += 1 {
+			path, present := m.Maps[image.Point{x, y}]
 
-			path, any := m.maps[image.Point{x, y}]
-			if any {
+			if present {
 				file, err := os.Open(path)
 				if err != nil {
 					return nil, err
 				}
 
 				scanner := bufio.NewScanner(file)
-				for scanner.Scan() {
-					line := scanner.Text()
-					if line == LEVEL {
-						break
-					}
-				}
-				if err = scanner.Err(); err != nil {
+				if err := skipToLevel(scanner); err != nil {
 					return nil, err
 				}
 
 				scanner.Scan()
 				data := strings.NewReader(scanner.Text())
 
-				var visited, length, position int
-				for {
-					n, err := fmt.Fscanf(data, "%d %d", &visited, &length)
-					if err != nil && err != io.EOF {
-						return nil, err
-					}
-					if n != 2 {
-						break
-					}
-
-					if visited == 1 {
-						x0 := position % MAPX
-						y0 := position / MAPX
-						x1 := (position + length - 1) % MAPX
-						y1 := (position + length - 1) / MAPX
-
-						if y0 == y1 {
-							// simple same-line case
-							draw_rect(img, CFG, draw.Src, ix, iy, x0, y0, x1, y1)
-						} else {
-							// multi-line case, try to make big blocks if possible
-							draw_rect(img, CFG, draw.Src, ix, iy, x0, y0, MAPX-1, y0)
-							if y0+1 != y1 {
-								draw_rect(img, CFG, draw.Src, ix, iy, 0, y0+1, MAPX-1, y1-1)
-							}
-							draw_rect(img, CFG, draw.Src, ix, iy, 0, y1, x1, y1)
-						}
-					}
-					position += length
+				boxes, err := parseSeen(data)
+				if err != nil {
+					return nil, err
+				}
+				for _, box := range boxes {
+					transformBox(&box, ix, iy)
+					draw.Draw(img, box, Config.Colors[FG], image.ZP, draw.Src)
 				}
 
 				scanner.Scan() // E 10
 				scanner.Scan() // 0 32400
 
-				var nx, ny int
-				for scanner.Scan() {
-					data = strings.NewReader(scanner.Text())
-					n, _ := fmt.Fscanf(data, "N %d %d", &nx, &ny)
-					if n != 2 {
-						break
-					}
-
-					draw_rect(img, CNOTE, draw.Over, ix, iy, nx, ny, nx, ny)
-
-					scanner.Scan() // skip note text
+				for _, box := range parseNotes(scanner) {
+					transformBox(&box, ix, iy)
+					draw.Draw(img, box, Config.Colors[NOTE], image.ZP, draw.Src)
 				}
 
 				file.Close()
 			}
 
-			// draw grid/origin
-			var c image.Image
 			if x == 0 && y == 0 {
-				c = CORIGIN
+				drawGrid(img, Config.Colors[ORIGIN], ix, iy)
 			} else {
-				c = CGRID
+				drawGrid(img, Config.Colors[GRID], ix, iy)
 			}
-
-			draw_rect(img, c, draw.Over, ix, iy, 0, 0, MAPX-1, 0)           // top-edge
-			draw_rect(img, c, draw.Over, ix, iy, 0, MAPY-1, MAPX-1, MAPY-1) // bottom-edge
-			draw_rect(img, c, draw.Over, ix, iy, 0, 0, 0, MAPY-1)           // left-edge
-			draw_rect(img, c, draw.Over, ix, iy, MAPX-1, 0, MAPX-1, MAPY-1) // right-edge
 
 			ix += 1
 		}
+
 		iy += 1
 	}
 
 	return img, nil
-}
-
-func draw_rect(img draw.Image, color image.Image, mode draw.Op, ix, iy, x0, y0, x1, y1 int) {
-	x0 = (ix * xo) + (x0 * SCALE)
-	y0 = (iy * yo) + (y0 * SCALE)
-	x1 = (ix * xo) + ((x1 + 1) * SCALE)
-	y1 = (iy * yo) + ((y1 + 1) * SCALE)
-	draw.Draw(img, image.Rect(x0, y0, x1, y1), color, image.ZP, mode)
-}
-
-func main() {
-	m, err := NewMap(os.Args[1])
-	if err != nil {
-		panic(err)
-	}
-
-	i, err := m.Draw()
-	if err != nil {
-		panic(err)
-	}
-
-	o, err := os.Create("output.png")
-	if err != nil {
-		panic(err)
-	}
-	if err := png.Encode(o, i); err != nil {
-		panic(err)
-	}
-	o.Close()
 }
